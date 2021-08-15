f728d3cc3902b8736b6f1eb803a583d227051df6
nathan.hsieh <yi82121@gmail.com>
2021-08-16 00:04:42 +0800

    feat: add crm user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,25 @@ INSERT INTO company (name, description, industry) VALUES
   ('Spotify', 'streaming tech company', 'Entertainment'),
   ('Amazon', 'platform', 'Paas'),
   ('Apple', 'tech company', 'Tech');
+
+
+
+------------------------------
+
+DROP TABLE IF EXISTS crm_user;
+
+CREATE TABLE crm_user (
+id INT AUTO_INCREMENT  PRIMARY KEY,
+email VARCHAR(250) NOT NULL,
+username VARCHAR(250) NOT NULL,
+password VARCHAR(250),
+role VARCHAR(250)
+);
+
+
+{
+  "email": "adminA@test.com",
+  "password": "password",
+  "role": "ADMIN",
+  "username": "ADMIN_A"
+}
